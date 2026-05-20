6b8e6f4c0dc1ab027414eb37b65da0084b288f64
Charon3000505 <erlandidn@gmail.com>
2026-05-20 08:42:09 +0700

    setup Laravel  auth  model  migration  relasi

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,11 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
+
     /**
      * Get the attributes that should be cast.
      *
